dbc2f28cf0322ea2d36aba6d49f9c1f51742f2eb
Alexander Kiderov <alexkiderov97@gmail.com>
2020-11-28 21:42:57 +0100

    Initial commit

---
+++ src/main/java/evans18/lunatechairportsbackend/LunatechAirportsBackendApplication.java
@@ -0,0 +1,13 @@
+package evans18.lunatechairportsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunatechAirportsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunatechAirportsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/evans18/lunatechairportsbackend/LunatechAirportsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package evans18.lunatechairportsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunatechAirportsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
